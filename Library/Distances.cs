ae55d1103bf510993749aecc0776365374b80e58
Andy <onsubmit@outlook.com>
2022-04-13 19:02:56 -0700

    Add Distances class

---
+++ Library/Distances.cs
@@ -0,0 +1,46 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Distances.cs" company="Andy Young">
+//     Copyright (c) Andy Young. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace Library
+{
+    /// <summary>
+    /// Used to record the distance of each cell from the start point.
+    /// </summary>
+    public class Distances
+    {
+        private readonly Dictionary<Cell, int> cells = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Distances"/> class.
+        /// </summary>
+        /// <param name="root">The root cell used to record the distance of each cell from it.</param>
+        public Distances(Cell root)
+        {
+            this.cells[root] = 0;
+        }
+
+        /// <summary>
+        /// Gets the cells for which the distance between them and the root is known.
+        /// </summary>
+        public Cell[] Cells => this.cells.Keys.ToArray();
+
+        /// <summary>
+        /// Gets or sets the distance between the root and the given cell.
+        /// </summary>
+        /// <param name="cell">The cell.</param>
+        public int this[Cell cell]
+        {
+            get
+            {
+                return this.cells[cell];
+            }
+
+            set
+            {
+                this.cells[cell] = value;
+            }
+        }
+    }
+}
